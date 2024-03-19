72eabcb6f7802be30b0bc1a5b311253dd5885c2a
ryouhei <@ryouhei_0705>
2024-03-19 16:20:01 +0900

    フロント側にて画面アクセス時にデータ取得処理の実装途中, Unhandled Runtime Errorが3つ出ている

--- everyone-book-tower-frontend/pages/index.tsx
+++ everyone-book-tower-frontend/pages/index.tsx
@@ -3,22 +3,71 @@ import { Inter } from 'next/font/google'
 import { useState } from "react";
 
 const inter = Inter({ subsets: ['latin'] })
+const BASE_URL: string = "api_url/api";
+const BOOKS_ENDPOINT: string = "/books";
+const BOOKS_READ_ENDPOINT: string = "/books/all";
 
+// 本のデータの型
 type Book = {
   name: string;
   day: string;
 }
 
-export default function Home() {
-  const [books, setBooks] = useState<Book[]>([
-      {
-        name: '流星の絆',
-        day: '2024/02/28/23:54:50',
-      }, {
-        name: 'きんフレ',
-        day: '2024/02/29/03:14:50',
-      }
-    ]);
+// propsの型
+type Props = {
+  initialBooks: Book[];
+}
+
+// 読書データを追加する関数
+const createBooks = async () => {
+  const response = await fetch(BASE_URL+BOOKS_ENDPOINT);
+  const res = await response.json();
+  return res;
+}
+
+// 読書データをとってくる関数
+const readBooks = async () => {
+  // const response = await fetch(BASE_URL + BOOKS_READ_ENDPOINT);
+  // const res = await response.json();
+  // return res;
+  return [
+    {
+      name: '流星の絆',
+      day: '2024/02/28/23:54:50',
+    }, {
+      name: 'きんフレ',
+      day: '2024/02/29/03:14:50',
+    }
+  ];
+}
+
+// 読書データを更新する関数
+const updateBooks = async () => {
+  const response = await fetch(BASE_URL + BOOKS_ENDPOINT);
+  const res = await response.json();
+  return res;
+}
+
+// 読書データを削除する関数
+const deleteBooks = async () => {
+  const response = await fetch(BASE_URL + BOOKS_ENDPOINT);
+  const res = await response.json();
+  return res;
+}
+
+export default function Home(props: Props) {
+  // 読書データ
+  const [books, setBooks] = useState<Book[]>(props.initialBooks);
+
+  // const [books, setBooks] = useState<Book[]>([
+  //   {
+  //     name: '流星の絆',
+  //     day: '2024/02/28/23:54:50',
+  //   }, {
+  //     name: 'きんフレ',
+  //     day: '2024/02/29/03:14:50',
+  //   }
+  // ]);
 
   // setBooks([
   //   {
@@ -32,9 +81,13 @@ export default function Home() {
   //     day: '2024/03/29/03:14:50',
   //   }
   // ]);
+  console.log("in Home");
+  console.log(books);
+  console.log(typeof books);
 
   return (
     <table>
+      {/* 読書データを一覧表示 */}
       {books && books.map((book: Book) => (
         <tr>
           <td>{book.name}</td>
@@ -48,3 +101,14 @@ export default function Home() {
     </table>
   )
 }
+
+export const getServerSideProps = async () => {
+  const res = await readBooks();
+  console.log("in getServerSideProps");
+  console.log(res);
+  return {
+    props: {
+      initialBooks: res
+    }
+  };
+};
